883aaa9f5315ae259e857b427d002f3a54a07867
Mariusz Kuc <mkucpl@gmail.com>
2015-10-21 21:07:32 +0200

    initial commit

---
+++ src/application/FXCalc.java
@@ -0,0 +1,61 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+
+public class FXCalc extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("fxcalc.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("fxscene.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("FXCalc");
+			primaryStage.setMinWidth(150);
+			primaryStage.setMinHeight(100);
+			primaryStage.setMaxWidth(600);
+			primaryStage.setMaxHeight(400);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	Double currentResult;
+	
+	@FXML
+	public void Click(ActionEvent event) {
+		String eventText = ((Button)event.getSource()).getText();
+		System.out.println(eventText);
+		//if(eventText.equalsIgnoreCase(anotherString))
+		display.appendText(eventText);
+		
+	}
+	@FXML
+	private TextField display;
+	
+	private Double addTwoNumbers(final Double firstNumber, final Double secondNumber) throws RuntimeException{
+		Double result = firstNumber +secondNumber;
+		if(result.isNaN()){
+			throw new RuntimeException("Overflow during addition");
+		}
+		else{
+			return result;
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
